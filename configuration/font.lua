a1da8d55805bb84555aa90713baa581116279526
KnightChaser <agerio100@naver.com>
2025-07-30 11:21:36 +0900

    chore: adjust font size and terminal app size

--- configuration/font.lua
+++ configuration/font.lua
@@ -4,7 +4,7 @@ local wezterm = require("wezterm")
 return {
 	font = wezterm.font("0xProto Nerd Font"),
 	warn_about_missing_glyphs = false,
-	font_size = 15,
-	initial_cols = 110,
-	initial_rows = 40,
+	font_size = 13,
+	initial_cols = 150,
+	initial_rows = 50,
 }
