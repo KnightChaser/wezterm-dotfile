cebd612f87c47af3c575f8e8305a7e3bb51cc4d2
knightchaser <agerio100@naver.com>
2025-08-08 19:22:47 +0900

    fix: set fallback fonts for Korean characters

--- configuration/font.lua
+++ configuration/font.lua
@@ -2,7 +2,10 @@
 local wezterm = require("wezterm")
 
 return {
-	font = wezterm.font("0xProto Nerd Font"),
+	font = wezterm.font_with_fallback({
+		{ family = "0xProto Nerd Font", weight = "Regular" }, -- Main font
+		{ family = "Noto Sans CJK KR", weight = "Regular" },
+	}),
 	warn_about_missing_glyphs = false,
 	font_size = 13,
 	initial_cols = 150,
